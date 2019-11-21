4698723418b6355b9078f01e37778908e148dee8
amartadey <amartamails@gmail.com>
2019-11-21 11:50:48 +0530

    Dynamic Typing without Word by Word Comparision

---
+++ Dynamic Typing/app.js
@@ -0,0 +1,53 @@
+const wording = ['Have several random sentences generated and you will soon be able to see if they can help with your project.','Javascript is a fun language','This is not Sparta so do not get exicted','Hi, welcome to Habra, a town in West Bengal'];
+let startTime,endTime;
+
+const message = document.querySelector('.message');
+const playText = document.querySelector('textarea');
+const button = document.querySelector('button');
+
+button.addEventListener('click',function(){
+    if(this.innerText == "Start"){
+        playText.disabled=false;
+        playGame();
+    }
+    else if(this.innerText == "Done"){
+        playText.disabled = true;
+        button.innerText = "Start";
+        endPlay();
+    }
+});
+
+
+function endPlay(){
+    let date = new Date();
+    endTime = date.getTime();
+    let totalTime = ((endTime-startTime)/1000);
+    console.log(totalTime);
+    let str = playText.value;
+    let wordCount = wordCounter(str);
+    let speed = Math.round((wordCount/totalTime)*60);
+    console.log(speed);
+    let finalMessage = `You typed at ${speed} words per minutes`;
+    if( str != message.innerText){
+        finalMessage +=`<br>There were some errors.`
+    }
+    message.innerHTML = finalMessage;
+}
+
+function wordCounter(strWords) {
+    let response = strWords.split(" ").length;
+    console.log(response);
+    return response;
+}
+
+
+function playGame(){ 
+    let randomNum = Math.floor(Math.random()*wording.length);
+    message.innerText = wording[randomNum];
+    let date = new Date();
+    startTime = date.getTime();
+    button.innerText = "Done";
+    playText.focus();
+    playText.value="";
+
+}
