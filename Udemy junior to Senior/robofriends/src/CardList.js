a35ae838e022b4a7d43211d733c1723694156b7d
Amarta Dey <34670651+amartadey@users.noreply.github.com>
2021-01-24 23:59:34 +0530

    Junior to senior

---
+++ Udemy junior to Senior/robofriends/src/CardList.js
@@ -0,0 +1,12 @@
+import Card from "./Card";
+
+const CardList = ({ robots }) => {
+  const cardComponent = robots.map((user, i) => {
+    return (
+      <Card id={robots[i].id} name={robots[i].name} email={robots[i].email} />
+    );
+  });
+  return <>{cardComponent}</>;
+};
+
+export default CardList;
